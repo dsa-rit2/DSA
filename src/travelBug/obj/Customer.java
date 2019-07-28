c3edd278427f470e72e853f74867a7e938a15f30
Nchen118 <nchen118@yahoo.com>
2019-07-29 01:08:39 +0800

    bug fix

--- src/travelBug/obj/Customer.java
+++ src/travelBug/obj/Customer.java
@@ -1,4 +1,4 @@
-package objClass;
+package travelBug.obj;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -8,102 +8,110 @@ import java.io.PrintWriter;
 import java.io.Serializable;
 import java.util.Scanner;
 
-public class Customer extends Person implements Serializable{
+public class Customer extends Person implements Serializable {
+	private static final long serialVersionUID = 1L;
 	private int nextCustID = 1001;
 	private String password;
-	private int custID;	
-	
-	public Customer(String firstName,String lastName,String username,String password) {
-		super(firstName, lastName,username,password);
+	private int custID;
+
+	public Customer(String firstName, String lastName, String username, String password) {
+		super(firstName, lastName, username, password);
 	}
-	public Customer(String firstName,String lastName,String username,String password,int custID) {
-		super(firstName, lastName,username,password);
+
+	public Customer(String firstName, String lastName, String username, String password, int custID) {
+		super(firstName, lastName, username, password);
 		this.custID = custID;
 	}
-	
+
 	public int getNextCustID() {
-  		return nextCustID++;
+		return nextCustID++;
 	}
+
 	public boolean checkCustExist(String custID) {
-    	String fileName = "User.txt";
-    	Scanner sc = new Scanner(System.in);
-    	String line = null;
-    	boolean exist = false;
-    	int choice;
-    	try {
-    		//read file
-				FileReader fileReader = new FileReader(fileName);
-				BufferedReader bufferedReader = new BufferedReader(fileReader);
-				StringBuffer stringBuffer = new StringBuffer();
-				//read all from the file
-				while((line = bufferedReader.readLine()) != null) {
-					String[] result = line.split(":");
-					
-					if(custID.equals(result[0])) {
-						exist = true;
-						return exist;
-					}				
-					} bufferedReader.close();
-    	} catch (IOException e) {
+		String fileName = "User.txt";
+		Scanner sc = new Scanner(System.in);
+		String line = null;
+		boolean exist = false;
+		int choice;
+		try {
+			// read file
+			FileReader fileReader = new FileReader(fileName);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			StringBuffer stringBuffer = new StringBuffer();
+			// read all from the file
+			while ((line = bufferedReader.readLine()) != null) {
+				String[] result = line.split(":");
+
+				if (custID.equals(result[0])) {
+					exist = true;
+					return exist;
+				}
+			}
+			bufferedReader.close();
+		} catch (IOException e) {
 			System.out.println("ERROR!");
 		}
-    	return exist;
+		return exist;
 	}
-    public String toString(){
-    	return custID+":"+super.toString();
-    }
+
+	public String toString() {
+		return custID + ":" + super.toString();
+	}
+
 	public void registerCust() {
-    	String fileName = "User.txt";
-    	Scanner sc = new Scanner(System.in);
-    	String line = null;
-    	int choice;
-    	try {
-			//check file exists, yes append else create new text file
+		String fileName = "User.txt";
+		Scanner sc = new Scanner(System.in);
+		String line = null;
+		int choice;
+		try {
+			// check file exists, yes append else create new text file
 			FileWriter fw = new FileWriter(fileName, true);
 			PrintWriter pw = new PrintWriter(fw);
 			try {
 				FileReader fileReader = new FileReader(fileName);
 				BufferedReader bufferedReader = new BufferedReader(fileReader);
 				StringBuffer stringBuffer = new StringBuffer();
-				//read all from the file
-				while((line = bufferedReader.readLine()) != null) {
+				// read all from the file
+				while ((line = bufferedReader.readLine()) != null) {
 					nextCustID++;
 				}
 			} catch (IOException e) {
-			System.out.println("ERROR!");
+				System.out.println("ERROR!");
 			}
 			custID = getNextCustID();
-				pw.println(toString());
-			    pw.close();
-			    fw.close();	
-		}catch (IOException e) {
-			System.out.println("ERROR!");
-			}
-	}
-	public boolean checkLogin(String username,String password) {
-    	String fileName = "User.txt";
-    	Scanner sc = new Scanner(System.in);
-    	String line = null;
-    	boolean exist = false;
-    	int choice;
-    	try {
-    		//read file
-				FileReader fileReader = new FileReader(fileName);
-				BufferedReader bufferedReader = new BufferedReader(fileReader);
-				StringBuffer stringBuffer = new StringBuffer();
-				//read all from the file
-				while((line = bufferedReader.readLine()) != null) {
-					String[] result = line.split(":");
-					
-					if(username.equals(result[1])&&password.equals(result[2])) {
-						exist = true;
-						return exist;
-					}				
-					} bufferedReader.close();
-    	} catch (IOException e) {
+			pw.println(toString());
+			pw.close();
+			fw.close();
+		} catch (IOException e) {
 			System.out.println("ERROR!");
 		}
-    	return exist;
 	}
-	
+
+	public boolean checkLogin(String username, String password) {
+		String fileName = "User.txt";
+		Scanner sc = new Scanner(System.in);
+		String line = null;
+		boolean exist = false;
+		int choice;
+		try {
+			// read file
+			FileReader fileReader = new FileReader(fileName);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			StringBuffer stringBuffer = new StringBuffer();
+			// read all from the file
+			while ((line = bufferedReader.readLine()) != null) {
+				String[] result = line.split(":");
+
+				if (username.equals(result[1]) && password.equals(result[2])) {
+					exist = true;
+					return exist;
+				}
+			}
+			bufferedReader.close();
+		} catch (IOException e) {
+			System.out.println("ERROR!");
+		}
+		return exist;
+	}
+
 }
--- src/travelBug/obj/Person.java
+++ src/travelBug/obj/Person.java
@@ -1,47 +1,51 @@
-package objClass;
+package travelBug.obj;
 
 public class Person {
-protected String firstName;
-protected String lastName;
-protected String username;
-protected String password;
+	protected String firstName;
+	protected String lastName;
+	protected String username;
+	protected String password;
 
-public Person(String firstName,String lastName,String username,String password) {
-	this.firstName = firstName;
-	this.lastName = lastName;
-	this.username = username;
-	this.password = password;
-}
-
-public void setFirstName(String firstName){
-	this.firstName = firstName;
+	public Person(String firstName, String lastName, String username, String password) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
 	}
 
-public String getFirstName(){
-	return firstName;
-	}
-	
-public void setLastName(String lastName){
-	this.lastName = lastName;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
 
-public String getLastName(){
-	return lastName;
+	public String getFirstName() {
+		return firstName;
 	}
-public void setUsername(String username) {
-	this.username = username;
-}
-public String getUsername() {
-	return username;
-}
-public void setPassword(String password) {
-	this.password = password;
-}
-public String getPassword() {
-	return password;
-}
 
-public String toString(){
-	return username +":"+password +":"+ firstName +":"+ lastName +":";
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String toString() {
+		return username + ":" + password + ":" + firstName + ":" + lastName + ":";
 	}
 }

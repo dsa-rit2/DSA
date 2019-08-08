b34068fb8cd9b080c51086c0c5a82a60171f7744
Lim Zhi Yang <Lim Zhi Yang@DESKTOP-3C90DIC>
2019-08-08 22:54:59 +0800

    This is the circular link and view trip(part E), need to wait plan trip

--- src/travelBug/UI/UIControl.java
+++ src/travelBug/UI/UIControl.java
@@ -29,7 +29,7 @@ public class UIControl extends JFrame {
 
 		// ============= Change Panel Section =============
 //		changePanel(new AddLocation(this));
-		changePanel(new PlanTrip(this));
+//		changePanel(new PlanTrip(this));
 //		changePanel(new TravelLegMaintenance(this));
 //		changePanel(new AddCompany(this));
 //		changePanel(new ListCompany(this));
@@ -43,6 +43,7 @@ public class UIControl extends JFrame {
 //		changePanel(new AddTravelLeg(this));
 //		changePanel(new MainMenu(this));
 //		changePanel(new AdminReport(this));
+		changePanel(new ViewTrip(this));
 	}
 
 	public void changePanel(JPanel panelName) {

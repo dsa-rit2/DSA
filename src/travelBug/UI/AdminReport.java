1e3e64772a21830eb6eafe5cc43ef645ad0b810f
TEY YONG HUP <wesly123451@outlook.com>
2019-08-08 15:32:13 +0800

    123123

--- src/travelBug/UI/AdminReport.java
+++ src/travelBug/UI/AdminReport.java
@@ -14,6 +14,16 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+
+import org.omg.CORBA.portable.ValueBase;
+
+import travelBug.library.ReadWriteFile;
+import travelBug.library.singlyLinkedList;
+import travelBug.obj.Location;
+import travelBug.obj.SourceDest;
+import travelBug.obj.sourceDest2;
+
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
@@ -26,10 +36,23 @@ public class AdminReport extends JPanel {
 	private JTable table;
 	private DefaultTableModel tableModel;
 	private Vector<?> vector;
-
+	private JScrollPane scrollPane;
+//	private singlyLinkedList<SourceDest> hahalLinkedList = new singlyLinkedList<SourceDest>();
+//	private ReadWriteFile<SourceDest> lFile = new ReadWriteFile<SourceDest>("SourceDes.txt", SourceDest.class);
+	private singlyLinkedList<sourceDest2> hahalLinkedList2 = new singlyLinkedList<sourceDest2>();
+	private ReadWriteFile<sourceDest2> lFile2 = new ReadWriteFile<sourceDest2>("sourceDes.txt2", sourceDest2.class);
+	
 	public AdminReport(UIControl parent) {
 		super();
 		this.mainFrame = parent;
+		SourceDest dest = new SourceDest("asd", "pp");
+		dest.addCount();
+		//read txtile
+		hahalLinkedList2.add(new sourceDest2("aaa", "ppqqq"));
+		hahalLinkedList2.add(new sourceDest2("wwwww", "ppqqq"));
+		hahalLinkedList2.add(new sourceDest2("qqqq", "ppqqq"));
+		hahalLinkedList2.add(new sourceDest2("rrrrr", "ppqqq"));
+		hahalLinkedList2.add(new sourceDest2("qqqq", "ppqqq"));
 		setBackground(new Color(0, 0, 0, 0));
 		setBounds(new Rectangle(new Dimension(900, 450)));
 		setLayout(null);
@@ -48,11 +71,6 @@ public class AdminReport extends JPanel {
 				return false;
 			}
 		};
-
-		table = new JTable();
-		table.setBackground(Color.WHITE);
-		table.setBounds(127, 358, 633, -242);
-		add(table);
 		table = new JTable(tableModel);
 		table.setFont(new Font("Segoe UI", Font.PLAIN, 16));
 		table.addMouseListener(new MouseAdapter() {
@@ -73,13 +91,102 @@ public class AdminReport extends JPanel {
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		table.getTableHeader().setReorderingAllowed(false);
 		table.getTableHeader().setResizingAllowed(false);
-		
-		tableModel.addColumn("Location");
-		tableModel.addColumn("Continent");
-		tableModel.addColumn("State");
-		tableModel.addColumn("Country");
-		tableModel.addColumn("Type");
-		tableModel.addColumn("Number of Choosen");
+		table.setBounds(34, 31, 537, 167);
+		table.setRowHeight(100);
+//		contentPane.add(table);
+		scrollPane = new JScrollPane(table);
+		scrollPane.setBounds(12, 93, 876, 297);
+		scrollPane.setEnabled(false);
+		add(scrollPane);
 
+		for (int i = 0; i < table.getColumnCount(); i++) {
+			int j = 0;
+			TableColumn column = table.getColumnModel().getColumn(i);
+			if (i >= 0) {
+				if (i == j) {
+					column.setPreferredWidth(100);
+				}
+			}
+			j++;
+		}
+		tableModel.addColumn("Source");
+		tableModel.addColumn("Destionation");
+		tableModel.addColumn("Number of Visit");
+
+		load(null);
+		for (int i = 0; i < table.getColumnCount(); i++) {
+			int j = 0;
+			TableColumn column = table.getColumnModel().getColumn(i);
+			if (i >= 0) {
+				if (i == j)
+					column.setPreferredWidth(100);
+				
+			}
+			j++;
+		}
+	}
+
+	public void load(int month , int year,String Tdestination,String Tsource) {
+		int count = 1;
+		String [] location = new String[1000];
+		String []destination = new String[1000];
+		
+		tableModel.setRowCount(0);
+		
+		for(int j=1; j <= hahalLinkedList2.getNumberOfEntries() ; j++) {
+			 location [j]=hahalLinkedList2.getEntry(j).getL1();
+			 destination[j] = hahalLinkedList2.getEntry(j).getL2();
+		}
+		
+		
+		for(int i =1; i<= hahalLinkedList2.getNumberOfEntries() ; i++) {
+			          
+			if(month == hahalLinkedList2.getEntry(i).getLocalDate().getMonth() && year == hahalLinkedList2.getEntry(i).getLocalDate().getYear() ) {
+				if(location[i]= Tsource && destionation[i] = Tdestination) {
+					location[i] = null;
+					destionation[i]= null;
+					count++;
+				}
+			}
+		}
+//		if (anyString == null) {
+//			for (int i = 0; i < hahalLinkedList.getNumberOfEntries(); i++) {
+//				String[] dataStrings = {hahalLinkedList.getEntry(i).getL1(),hahalLinkedList.getEntry(i).getL2(),Integer.toString(hahalLinkedList.getEntry(i).getCount())
+//						};
+//				tableModel.addRow(dataStrings);
+//			}
+//		} else {
+//			anyString = anyString.toUpperCase();
+//			for (int i = 0; i < hahalLinkedList.getNumberOfEntries(); i++) {
+//				if (hahalLinkedList.getEntry(i).getL1().toUpperCase().matches(anyString + ".*")) {
+//					String[] dataStrings = {hahalLinkedList.getEntry(i).getL1(),
+//							hahalLinkedList.getEntry(i).getL2(),Integer.toString(hahalLinkedList.getEntry(i).getCount())};
+//					tableModel.addRow(dataStrings);
+//				}
+//
+//			}
+//		}
+		//
+		for (int i = 1; i <= hahalLinkedList.getNumberOfEntries(); i++) {
+			String[] dataStrings = { hahalLinkedList.getEntry(i).getL1(), hahalLinkedList.getEntry(i).getL2(),
+					Integer.toString(hahalLinkedList.getEntry(i).getCount()) };
+			tableModel.addRow(dataStrings);
+		}
+	}
+	
+	// put the function below to the search button
+	public void compare(String source,String destionation) {
+		//read data from txt file
+		boolean found = false;
+		for(int i =1 ; i<= hahalLinkedList2.getNumberOfEntries(); i ++) {
+			if(hahalLinkedList2.getEntry(i).getL1()== source && hahalLinkedList2.getEntry(i).getL2() == destionation) {
+//				hahalLinkedList2.getEntry(i).addCount();
+//				found = true;
+			}
+		}
+		if(!found) {
+			hahalLinkedList2.add(new sourceDest2(source,destionation));
+		}
+		// write into text field
 	}
 }
---
+++ src/travelBug/UI/Test2.java
@@ -0,0 +1,5 @@
+package travelBug.UI;
+
+public class Test2 {
+
+}

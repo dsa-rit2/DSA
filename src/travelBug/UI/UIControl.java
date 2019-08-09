bc6c7f082e622dfbc21591c3e3aa83fdedc52852
Nchen118 <nchen118@yahoo.com>
2019-08-10 02:51:29 +0800

    asd

--- src/travelBug/UI/UIControl.java
+++ src/travelBug/UI/UIControl.java
@@ -28,39 +28,7 @@ public class UIControl extends JFrame {
 		getContentPane().setLayout(null);
 
 		// ============= Change Panel Section =============
-<<<<<<< HEAD
-//		changePanel(new TravelLegMaintenance(this));
-//		changePanel(new PlanTrip(this));
-//		// ============= Change Panel Section =============
-//	//	changePanel(new ListLocation(this));
-//		changePanel(new AddLocation(this));
-//		changePanel(new AddTravelLeg(this));
-//		changePanel(new AddLocation(this));
-//		changePanel(new TravelLegMaintenance(this));
-//		changePanel(new PlanTrip(this));
-//		changePanel(new AddLocation(this));
-		changePanel(new TravelLegMaintenance(this));
-//		changePanel(new AddCompany(this));
-//		changePanel(new ListCompany(this));
-//		changePanel(new AddTravelLeg(this));
-//		changePanel(new Authentication(this));
-//		changePanel(new ListLocation(this));
-//		changePanel(new AddTravelLeg(this));
-
-//	changePanel(new AddTravelLeg(this));
-
-//		changePanel(new AdminReport(this));
-		changePanel(new TravelLegMaintenance(this));
-//		changePanel(new MainMenu(this));
-
-//		changePanel(new AdminReport(this));
-
-//		changePanel(new AdminReport(this));
-//		changePanel(new ViewTrip(this));
-//		changePanel(new PlanTrip(this));
-=======
 		changePanel(new PlanTrip(this));
->>>>>>> branch 'master' of https://github.com/dsa-rit2/DSA.git
 	}
 
 	public void changePanel(JPanel panelName) {

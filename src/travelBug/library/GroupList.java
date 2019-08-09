87b87f203276df7b6b4863c7f0f4337c423522de
Nchen118 <nchen118@yahoo.com>
2019-08-09 14:17:57 +0800

    group fix

--- src/travelBug/library/GroupList.java
+++ src/travelBug/library/GroupList.java
@@ -19,51 +19,33 @@ public class GroupList<T, E> implements GroupListInterface<T, E> {
 		this();
 		this.c = c;
 
-		SortedLinkedList<T> sortedLinkedList = new SortedLinkedList<T>(listNode, c);
-		SinglyLinkedList<T> tempLinkedList = new SinglyLinkedList<T>();
+		if (!listNode.isEmpty()) {
+			SortedLinkedList<T> sortedLinkedList = new SortedLinkedList<T>(listNode, c);
+			SinglyLinkedList<T> tempLinkedList = new SinglyLinkedList<T>();
 
-//		System.out.println(tempLinkedList.getNumberOfEntries());
-		sortedLinkedList.forEach(list -> {
-//			System.out.println(list);
-		//	if (tempLinkedList.isEmpty() || (c.compare(list, tempLinkedList.getFirst())) == 0) {
-			//	tempLinkedList.add(list);
-//				System.out.println("1.**" + tempLinkedList.getNumberOfEntries());
-		//	}
-		//	else {
-				//this.add((E) tempLinkedList);
-//				System.out.println(tempLinkedList.getNumberOfEntries());
-			//	tempLinkedList.clear();
-				//tempLinkedList.add(list);
-//				System.out.println("Element: " + firstNode.data);
-		//	}
-//			System.out.println(tempLinkedList.getFirst().toString());
-			;
-		});
-		//this.add((E) tempLinkedList);
-
-//		System.out.println(tempLinkedList.getFirst().toString());
-//		System.out.println(tempLinkedList.getFirst());
-//		System.out.println("Number of Pointer: " + numberOfEntries);
-		
-
-		for (T list : sortedLinkedList) {
-			if (!tempLinkedList.isEmpty() && (c.compare(list, tempLinkedList.getFirst())) != 0) {
-				this.addGroup((E) tempLinkedList);
-				tempLinkedList = new SinglyLinkedList<T>();
+			for (T list : sortedLinkedList) {
+				if (!tempLinkedList.isEmpty() && (c.compare(list, tempLinkedList.getFirst())) != 0) {
+					this.addGroup((E) tempLinkedList);
+					tempLinkedList = new SinglyLinkedList<T>();
+				}
+				tempLinkedList.add(list);
 			}
-			tempLinkedList.add(list);
+			this.addGroup((E) tempLinkedList);
 		}
-		this.addGroup((E) tempLinkedList);
 
 	}
 
+	@SuppressWarnings("unchecked")
 	public SinglyLinkedList<T> findChild(T data) {
 		SinglyLinkedList<T> tempLinkedList = new SinglyLinkedList<T>();
-		
-		for (SinglyLinkedList<T> list : this) {
-			
+
+		for (E list : this) {
+			SinglyLinkedList<T> item = (SinglyLinkedList<T>) list;
+			if (c.compare(data, item.getFirst()) == 0) {
+				tempLinkedList = item;
+			}
 		}
-		
+
 		return tempLinkedList;
 	}
 
